ea91e67a7d824aa84015e4c8cad2c909ad37b0c7
bihi111 <40891471+bihi111@users.noreply.github.com>
2018-07-11 23:55:06 +0000

    Add files via upload

---
+++ pyth.py
@@ -0,0 +1,16 @@
+def f():
+    print ("*********************************************++****************************")
+    r=eval(input("  to continue in english  : press (1) ** ** to continue in french : press (2)\n "))
+    if r==1:
+        m=(input("what's your name :\n"))
+
+        a=eval(input("how old are you :\n"))
+
+        n=(input("are you male or female :\n"))
+
+        c=(input( "where are you from \n(to benefit from docfinder must be among these cities) :"))
+        print("*****************************************************************")
+        print ("to use docfinder you must follow these steps:")
+        
+
+    
